56432f866f9fead67c65c58fa920cbe66e834487
abdulrehman ajmal <abdulrehmanajmal@abdulrehmans-MacBook-Pro.local>
2022-08-02 16:23:19 +0500

    argv added

--- hook/main.py
+++ hook/main.py
@@ -1,30 +1,29 @@
 import argparse
-import os 
+import os
+from typing import Sequence
 
 
-def print_arguments(arguments):
-    for argument in arguments:
-        print(argument)
 
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path")
-    args = parser.parse_args()
-    MIGRATION_DIR = args.path
-    filenames = os.listdir(MIGRATION_DIR)
-    filenames.remove('__init__.py')
-    checklist = []
-
-    for filename in filenames:
-        arr = filename.split("_")
-        checklist.append(arr[0])
+def main(argv: Sequence[str] | None = None):
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("path")
+    # args = parser.parse_args()
+    # MIGRATION_DIR = args.path
+    # filenames = os.listdir(MIGRATION_DIR)
+    # filenames.remove('__init__.py')
+    # checklist = []
+    # for filename in filenames:
+    #     arr = filename.split("_")
+    #     checklist.append(arr[0])
     
 
-    if len(set(checklist)) == len(checklist):
-        pass
-    else:
-        raise ValueError("Duplicates In Migrations .")
+    # if len(set(checklist)) == len(checklist):
+    #     pass
+    # else:
+    #     raise ValueError("Duplicates In Migrations .")
+    
+    print(argv)
+
         
 
 
@@ -32,4 +31,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
